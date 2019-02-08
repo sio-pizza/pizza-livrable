17f4cffb57280911f5c3f196ab18cb72b2089b43
ThomasAronica <44808423+ThomasAronica@users.noreply.github.com>
2019-02-08 15:37:53 +0100

    Add files via upload

---
+++ launcher.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>launcher</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="icon" type="image/png" href="icon.png">
+    </head>
+    <body>
+            <form action="gestliv.php" method="GET" enctype="multipart/form-data">
+            <p>Welcome to pizza sio</p>
+            <input type="button" value="Démarrer" class="bouton">
+            </form>
+    </body>
+</html>
